20b53e134fd728e98eecaeacb059e743b385dea4
vertoxp89 <147354458+vertoxp89@users.noreply.github.com>
2024-11-07 01:30:38 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+rel="stylesheet" integrity="sha384-
+QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+crossorigin="anonymous">
+<title>Banac Ubunto Server-PHP </title>
+</head>
+<body>
+<center>
+<p>Banac Ubunto Server-PHP </p> <br>
+
+<div class="container">
+<label id="yawa">Firstname:</label>
+<input id="yawa" type="text" name="firstname">
+</div><br>
+
+<div class="container">
+
+<label>Middlename:</label>
+<input type="text" name="firstname">
+</div><br>
+
+<div class="container">
+
+<label>Lastname:</label>
+<input type="text" name="lastname">
+</div><br>
+
+<div class="container">
+<label>Age:</label>
+<input type="text" name="age">
+</div><br>
+
+<div class="container">
+<label>Address:</label>
+<input type="text" name="address">
+</div><br>
+
+<div class="container">
+
+<label>Course and Section:</label>
+<input type="text" name="course">
+</div><br>
+
+<div>
+<label>Submit:</label>
+<input type="submit" name="course">
+</div><br>
+
+</center>
+</body>
+</html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+crossorigin="anonymous"></script>
